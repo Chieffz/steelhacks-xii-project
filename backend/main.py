ac034d26483a772576711f39792afee2c5acfba7
Chieffz <bitmod3@gmail.com>
2025-09-21 10:14:58 -0400

    fixed cors

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from fastapi import FastAPI, Depends
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi.middleware.cors import CORSMiddleware
 
 from .endpoints import form, auth
 
@@ -15,6 +16,14 @@ app.include_router(form.router,
 app.include_router(auth.router,
                    prefix="/auth",
                    tags=["auth"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
